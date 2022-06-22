79bdf76062f1e64c0c36f7c1f0217c15f70eebca
keke <keke@mail.com>
2022-06-23 00:22:04 +0800

    支持heic格式,忽略隐藏文件夹

--- main.py
+++ main.py
@@ -38,23 +38,32 @@ def process_pic(src_dir, dst_dir):
     os.makedirs(dst_dir, exist_ok=True)
     abs_filenames = get_all_files(src_dir)
     for abs_filename in abs_filenames:
+        filename = os.path.basename(abs_filename)
+        if len(filename) > 0 and filename[0] == ".":  # 跳过隐藏文件
+            continue
         create_time = get_file_time(abs_filename)  # 获取目录下所有的照片列表
-        if create_time == "":
+        if not create_time:
             print("not find time", abs_filename)
+            continue
+        if len(create_time) < 6:  # 20181207_031034
+            print(f"create_time is not right, {create_time}, {abs_filename}")
             continue
         year = create_time[0:4]
         mounth = create_time[4:6]
         new_dir = os.path.join(dst_dir, year, mounth)
         os.makedirs(new_dir, exist_ok=True)
-        dst_filename = os.path.join(new_dir, os.path.basename(abs_filename))
-        if not os.path.exists(dst_filename):
-            shutil.move(abs_filename, dst_filename)
-        elif os.path.isfile(dst_filename):
-            if os.path.getsize(abs_filename) == os.path.getsize(dst_filename):
+        dst_abs_filename = os.path.join(new_dir, filename)
+        if not os.path.exists(dst_abs_filename):
+            shutil.move(abs_filename, dst_abs_filename)
+            print(f"mv {abs_filename} to {dst_abs_filename}")
+        elif os.path.isfile(dst_abs_filename):
+            if os.path.getsize(abs_filename) == os.path.getsize(dst_abs_filename):
                 os.remove(abs_filename)
             else:
-                print("file name same,but not same file")
-                shutil.move(abs_filename, os.path.join(new_dir, "same_" + abs_filename))
+                print(f"file name same,but not same file {abs_filename}")
+                dst_abs_filename = os.path.join(new_dir, "same_" + filename)
+                shutil.move(abs_filename, dst_abs_filename)
+                print(f"mv {abs_filename} to {dst_abs_filename}")
         else:
             print("same dir name is exist.")
 
